06591c91c2ff8d90a3b6cf18f024cafda1a5670c
akj <akj@bbtsoftware.ch>
2017-03-15 14:37:46 +0100

    project structure set up

---
+++ DbServerPluginMsSql2014/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbServerPluginMsSql2014
+{
+    public class Class1
+    {
+    }
+}
