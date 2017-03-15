6c3279da9acaf001031853646da9172582b0eca8
akj <akj@bbtsoftware.ch>
2017-03-15 14:45:19 +0100

    Exception type added

---
+++ DbServerPlugin/SnapshotException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DbServerPlugin
+{
+    using System.Runtime.CompilerServices;
+
+    public class SnapshotException : Exception
+    {
+        public SnapshotException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
